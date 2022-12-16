9d30c94f8f10fc572defada2ef59cd93c9392e8c
RajatsGit <115529699+RajatsGit@users.noreply.github.com>
2022-12-17 03:17:45 +0530

    Create Simple_Input_String_Integer.java

---
+++ DSA_BASICS-1/Simple_Input_String_Integer.java
@@ -0,0 +1,17 @@
+Given a string S and Integer n, your task is to take input of the given string and print the same string and Integer value.
+  
+  CODE IMPLEMENATATION <---------------->
+  import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+ Scanner str = new Scanner(System.in);
+ String ut = str.nextLine();
+ int u = str.nextInt();     
+ System.out.println(ut);
+ System.out.println(u);
+    }
+}
