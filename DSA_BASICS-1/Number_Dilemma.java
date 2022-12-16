fab7da33930df4bf5f6dc81c71ac68601e085176
RajatsGit <115529699+RajatsGit@users.noreply.github.com>
2022-12-17 03:06:04 +0530

    Update Number_Dilemma.java

--- DSA_BASICS-1/Number_Dilemma.java
+++ DSA_BASICS-1/Number_Dilemma.java
@@ -1,7 +1,7 @@
 Problem Statement
 Nobita likes a number if it is stored in an integer while Doraemon likes it when it is stored in a String. Your task is to write a code so that they can easily convert an integer to a string or a string to an integer whenever they want.
   
-Return an integer in StringToInt() while return a integer integer in IntToString(). The driver code will print "Nice Job" if your code is correct otherwise "Wrong answer".
+Return an integer in StringToInt() while return a integer in IntToString(). The driver code will print "Nice Job" if your code is correct otherwise "Wrong answer".
   
 Sample Input:-
 12
