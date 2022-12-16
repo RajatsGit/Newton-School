cf67dcdf7372510ca9ce533138ee29c7eb00d9a6
RajatsGit <115529699+RajatsGit@users.noreply.github.com>
2022-12-17 02:51:46 +0530

    Create String_TypeCast_Functional_Problem.java

---
+++ Month-June/String_TypeCast_Functional_Problem.java
@@ -0,0 +1,16 @@
+Problem Statement
+Given an integer N, you need to typecast this integer to String. If the typecasting is done successfully then we will print "Nice Job" otherwise "Wrong answer".
+The driver code will print "Nice Job" otherwise "Wrong answer".
+
+Sample Input:
+5
+
+Sample Output:
+Nice Job
+
+CODE IMPLEMENTATION <------------>
+static String checkConevrtion(int a)
+{
+String N = String.valueOf(a);  
+return N;   
+}
